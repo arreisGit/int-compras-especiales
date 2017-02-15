ba2a3d8f325f7c07d8b80059c070c9bd1c769e14
Enrique Sierra Gutierrez <enrique.sierra.0000@gmail.com>
2017-02-15 10:49:15 -0600

    Añade un script que puede ser usado para borrar las tablas del desarrollo en el orden correcto evitando conflictos

---
+++ SQL/Misc/let_there_be_light.sql
@@ -0,0 +1,8 @@
+IF OBJECT_ID('dbo.CUP_CriteriosComprasEspeciales', 'U') IS NOT NULL 
+  DROP TABLE dbo.CUP_CriteriosComprasEspeciales; 
+
+IF OBJECT_ID('dbo.CUP_AccionesComprasEspeciales', 'U') IS NOT NULL 
+  DROP TABLE dbo.CUP_AccionesComprasEspeciales 
+
+IF OBJECT_ID('dbo.CUP_EliminarSaldosMenoresInv_Escenarios', 'U') IS NOT NULL 
+  DROP TABLE dbo.CUP_RecurrenciaAccionComprasEspeciales; 
--- SQL/Tables/CUP_CriteriosComprasEspeciales.sql
+++ SQL/Tables/CUP_CriteriosComprasEspeciales.sql
@@ -6,6 +6,7 @@ CREATE TABLE dbo.CUP_CriteriosComprasEspeciales
   ID INT PRIMARY KEY IDENTITY(1,1) NOT NULL,
   ProvCatProductoServicio_ID HIERARCHYID
                              NULL
+                             CONSTRAINT FK_CUP_CriteriosComprasEspeciales_ProvCatProductoServicio
                              FOREIGN KEY
                              REFERENCES CUP_ProvCatProductoServicio(ID),
   ArtCategoria VARCHAR(50) NULL,
@@ -13,18 +14,22 @@ CREATE TABLE dbo.CUP_CriteriosComprasEspeciales
   Articulo CHAR(20) NULL,
   Largo VARCHAR(20) NULL,
   Accion_ID INT NOT NULL
+             CONSTRAINT FK_CUP_CriteriosComprasEspeciales_Accion
              FOREIGN KEY 
              REFERENCES CUP_AccionesComprasEspeciales(ID),
   Recurrencia_ID INT NOT NULL 
+                  CONSTRAINT FK_CUP_CriteriosComprasEspeciales_Recurrencia
                   FOREIGN KEY  
                   REFERENCES CUP_RecurrenciaAccionComprasEspeciales(ID),
   FechaInicio DATE NOT NULL,
   Activo BIT NOT NULL 
-         CONSTRAINT [DF_CUP_CUP_CriteriosComprasEspeciales_Activo] DEFAULT 1,
+         CONSTRAINT [DF_CUP_CUP_CriteriosComprasEspeciales_Activo]
+         DEFAULT 1,
   Descripcion VARCHAR(255) NOT NULL,
   Usuario INT NOT NULL,
   FechaRegistro DATETIME NOT NULL
-          CONSTRAINT [DF_CUP_CUP_CriteriosComprasEspeciales_FechaRegistro] DEFAULT GETDATE()
+                CONSTRAINT [DF_CUP_CUP_CriteriosComprasEspeciales_FechaRegistro]
+                DEFAULT GETDATE()
 ) 
 
 GO
