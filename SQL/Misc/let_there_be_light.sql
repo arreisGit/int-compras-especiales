5a4b9f4d643ff597ad76b2acfa93a3e229ddfaa2
Enrique Sierra Gutierrez <enrique.sierra.0000@gmail.com>
2017-02-15 17:31:15 -0600

    Añade la tabla CUP_ComprasEspeciales al script que borra los objetos en un orden para evitar conflicto de dependencias durante la liberacion

--- SQL/Misc/let_there_be_light.sql
+++ SQL/Misc/let_there_be_light.sql
@@ -1,8 +1,11 @@
+IF OBJECT_ID('dbo.CUP_ComprasEspeciales', 'U') IS NOT NULL 
+  DROP TABLE dbo.CUP_ComprasEspeciales; 
+
 IF OBJECT_ID('dbo.CUP_ComprasEspeciales_Criterios', 'U') IS NOT NULL 
   DROP TABLE dbo.CUP_ComprasEspeciales_Criterios; 
 
-IF OBJECT_ID('dbo.CUP_ComprasEspeciales_Acciones', 'U') IS NOT NULL 
-  DROP TABLE dbo.CUP_ComprasEspeciales_Acciones 
-
 IF OBJECT_ID('dbo.CUP_ComprasEspeciles_Recurrencias; ', 'U') IS NOT NULL 
   DROP TABLE dbo.CUP_ComprasEspeciles_Recurrencias; 
+
+IF OBJECT_ID('dbo.CUP_ComprasEspeciales_Acciones', 'U') IS NOT NULL 
+  DROP TABLE dbo.CUP_ComprasEspeciales_Acciones 
