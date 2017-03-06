6da77aa9ca00446e162884a535f36e997d8814f7
Enrique Sierra Gutierrez <enrique.sierra.0000@gmail.com>
2017-03-06 15:27:47 -0600

    Añade el respaldo del xpInvAfectarAntes

---
+++ SQL/Stored Procedures/xpInvAfectarAntes.sql
@@ -0,0 +1,37 @@
+USE [Cuprum]
+GO
+
+/****** Object:  StoredProcedure [dbo].[xpInvAfectarAntes]    Script Date: 06/03/2017 03:26:45 p.m. ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER OFF
+GO
+
+ALTER PROCEDURE [dbo].[xpInvAfectarAntes]
+@ID                		int,
+@Accion			char(20),
+@Base			char(20),
+@Empresa	      		char(5),
+@Usuario			char(10),
+@Modulo	      		char(5),
+@Mov	  	      		char(20),
+@MovID             		varchar(20),
+@MovTipo     		char(20),
+@MovMoneda	      		char(10),
+@MovTipoCambio	 	float,
+@Estatus	 	      	char(15),
+@EstatusNuevo	      	char(15),
+@FechaEmision		datetime,
+@FechaRegistro		datetime,
+@FechaAfectacion    		datetime,
+@Conexion			bit,
+@SincroFinal			bit,
+@Sucursal			int,
+@UtilizarID			int,
+@UtilizarMovTipo    		char(20),
+@Ok				int		OUTPUT,
+@OkRef			varchar(255)	OUTPUT
+AS BEGIN
+RETURN
+END
